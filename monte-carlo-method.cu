9519685390e2c009e474749245d4c934d99cb691
Michael Chase <themikechase@gmail.com>
2025-01-17 20:07:47 -0800

    adding gpu benchmark tests

--- monte-carlo-method.cu
+++ monte-carlo-method.cu
@@ -29,7 +29,6 @@
   // Paths: 8.192M
   // Steps: 365
 
-
 #define CHECKCURAND(expression)                         \
   {                                                     \
     curandStatus_t status = (expression);                         \
@@ -211,22 +210,144 @@ int main(int argc, char *argv[]) {
   }
 
 
+// Step 1: GPU memory allocated, initialized by CuPY array
+
+output = cupy.zeros(N_PATHS, dtype=cupy.float32)
+
+// Step 2: CuPy random function (cuRAND library), random number generation is defined as (below):
+
+randoms_gpu = cupy.random.normal(0, 1, N_PATHS * N_STEPS, dtype=cupy.float32)
+
+// Step 3: Execute GPU mean value composition (CuPy library) 
+
+v = output.mean()
+
+// Step 4: GPU memory deallocation (automated)
 
 
 
+// Execute Monte Carlo simulation on single core CPU, t=41.6s
 
+@njit(fastmath=True)
+def cpu_barrier_option(d_s, T, K, B, S0, sigma, mu, r, d_normals, N_STEPS, N_PATHS):
+    tmp1 = mu*T/N_STEPS
+    tmp2 = math.exp(-r*T)
+    tmp3 = math.sqrt(T/N_STEPS)
+    running_average = 0.0
+    for i in range(N_PATHS):
+        s_curr = 50
+        for n in range(N_STEPS):
+            s_curr += tmp1 * s_curr + sigma*s_curr*tmp3*d_normals[i + n * N_PATHS]
+            running_average = running_average + 1.0/(n + 1.0) * (s_curr - running_average)
+            if running_average <= B:
+                break
+        payoff = running_average - K if running_averages>K else 0
+        d_s[i] = tmp2 * payoff
 
+// Execute Monte Carlo simulation on single core CPU (DGX-1 Intel CPU)
 
+@njit(fastmath=True, parallel=True)
+def cpu_multiplecore_barrier_option(d_s, T, K, B, S0, sigma, mu, r, d_normals, N_STEPS, N_PATHS):
+    tmp1 = mu*T/N_STEPS
+    tmp2 = math.exp(-r*T)
+    tmp3 = math.sqrt(T/N_STEPS)
+    for i in prange(N_PATHS):
+        s_curr = S0
+        running_average = 0.0
+        for n in range(N_STEPS):
+            s_curr += tmp1 * s_curr + sigma*s_curr*tmp3*d_normals[i + n * N_PATHS]
+            running_average = running_average + 1.0/(n + 1.0) * (s_curr - running_average)
+            if running_average <= B:
+                break
+        payoff = running_average - K if running_average>K else 0
+        d_s[i] = tmp2 * payoff 
 
+// Numba library approach, single GPU V100 GPU; t=65ms
 
+@cuda.jit
+def numba_gpu_barrier_option(d_s, T, K, B, S0, sigma, mu, r, d_normals, N_STEPS, N_PATHS):
+    # ii - overall thread index
+    ii = cuda.threadIdx.x + cuda.blockIdx.x * cuda.blockDim.x
+    stride = cuda.gridDim.x * cuda.blockDim.x
+    tmp1 = mu*T/N_STEPS
+    tmp2 = math.exp(-r*T)
+    tmp3 = math.sqrt(T/N_STEPS)
+    running_average = 0.0
+    for i in range(ii, N_PATHS, stride):
+        s_curr = S0
+        for n in range(N_STEPS):
+            s_curr += tmp1 * s_curr + sigma*s_curr*tmp3*d_normals[i + n * N_PATHS]
+            running_average += (s_curr - running_average) / (n + 1.0)
+            if running_average <= B:
+                break
+        payoff = running_average - K if running_average>K else 0
+        d_s[i] = tmp2 * payoff 
 
+// CuPy library approach, single GPU, 29ms
 
+cupy_barrier_option = cupy.RawKernel(r'''
+extern "C" __global__ void barrier_option(
+    float *d_s,
+    const float T,
+    const float K,
+    const float B,
+    const float S0,
+    const float sigma,
+    const float mu,
+    const float r,
+    const float * d_normals,
+    const long N_STEPS,
+    const long N_PATHS)
+{
+  unsigned idx =  threadIdx.x + blockIdx.x * blockDim.x;
+  unsigned stride = blockDim.x * gridDim.x;
+  unsigned tid = threadIdx.x;
+ 
+  const float tmp1 = mu*T/N_STEPS;
+  const float tmp2 = exp(-r*T);
+  const float tmp3 = sqrt(T/N_STEPS);
+  double running_average = 0.0;
+ 
+  for (unsigned i = idx; i<N_PATHS; i+=stride)
+  {
+    float s_curr = S0;
+    unsigned n=0;
+    for(unsigned n = 0; n < N_STEPS; n++){
+       s_curr += tmp1 * s_curr + sigma*s_curr*tmp3*d_normals[i + n * N_PATHS];
+       running_average += (s_curr - running_average) / (n + 1.0) ;
+       if (running_average <= B){
+           break;
+       }
+    }
+ 
+    float payoff = (running_average>K ? running_average-K : 0.f);
+    d_s[i] = tmp2 * payoff;
+  }
+}
+ 
+''', 'barrier_option')
 
+// Multiple GPUs (Dask)
 
+def get_option_price(T, K, B, S0, sigma, mu, r, N_PATHS = 8192000, N_STEPS = 365, seed=3):
+    number_of_threads = 256
+    number_of_blocks = (N_PATHS-1) // number_of_threads + 1
+    cupy.random.seed(seed)
+    randoms_gpu = cupy.random.normal(0, 1, N_PATHS * N_STEPS, dtype=cupy.float32)
+    output =  cupy.zeros(N_PATHS, dtype=cupy.float32)
+    cupy_barrier_option((number_of_blocks,), (number_of_threads,),
+                   (output, np.float32(T), np.float32(K), 
+                    np.float32(B), np.float32(S0), 
+                    np.float32(sigma), np.float32(mu), 
+                    np.float32(r),  randoms_gpu, N_STEPS, N_PATHS))
+    v = output.mean()
+    out_df = cudf.DataFrame()
+    out_df['p'] = cudf.Series([v.item()])
+    return out_df 
 
-
-
-
+x = dask_cudf.from_delayed([delayed(get_option_price)(T=1.0, K=110.0,  B=100.0, S0=120.0, sigma=0.35, mu=0.1, r=0.05, seed=3000+i) for i in  range(1600)])
+x.mean().compute()
+x.std().compute() 
 
 
 
